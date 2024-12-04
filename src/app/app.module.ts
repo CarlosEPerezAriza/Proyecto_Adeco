177267e73a2fb5920f788c40beb60e19b638a9d1
Antonio-92-cpu <antoniooyuelasanchez@gmail.com>
2024-12-04 12:57:11 +0100

    Crear compomente tarjetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetasComponent
   ],
   imports: [
     BrowserModule
