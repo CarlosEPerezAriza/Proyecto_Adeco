26ead32696be96ac698996a90ab9560638faa05d
Alex Diaz (Electrofantasia) <mrplanning2@gmail.com>
2024-12-11 11:07:06 +0100

    card created

---
+++ src/app/home-cards/home-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-cards',
+  templateUrl: './home-cards.component.html',
+  styleUrls: ['./home-cards.component.css']
+})
+export class HomeCardsComponent {
+  imgSrc: string = "../../assets/sopa.jpg"
+  title: string = "Sopa casera"
+
+}
