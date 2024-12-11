c7c7eb239925b8a3c838fb43faefcad3c9e81b36
Alex Diaz (Electrofantasia) <mrplanning2@gmail.com>
2024-12-11 13:37:51 +0100

    data added + responsive grid

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,5 +11,8 @@ export class HomeComponent {
     new Recipe('Sopa casera', '../../assets/sopa.jpg'),
     new Recipe('Tortilla de patatas', '../../assets/tortilla-patatas.jpg'),
     new Recipe('Paella', '../../assets/paella.jpg'),
+    new Recipe('Bocata de jamón', '../../assets/bocata-jamon.jpg'),
+    new Recipe('Lentejas', '../../assets/lentejas.jpg'),
+    new Recipe('Cocido madrileño', '../../assets/cocido-madrileno.jpg'),
   ];
 }
